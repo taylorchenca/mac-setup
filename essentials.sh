5c616865284c698aab38d26999cba3cdc5a2c899
taylorchenca <taylorchenca@gmail.com>
2020-02-08 20:48:43 -0800

    Create essentials.sh

---
+++ essentials.sh
@@ -0,0 +1,23 @@
+# Install Homebrew 
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# brew list 
+gdbm		highlight	kubernetes-cli	libunistring	minikube	pkg-config	python@3.8	wget
+gettext		ipython		libidn2		lua		openssl		prometheus	readline	xz
+ghostscript	jpeg		libtiff		mackup		openssl@1.1	python		sqlite		zeromq
+
+# brew cask list
+amazon-chime               kindle                     spotify
+atom                       mactex                     squirrel
+dash                       qlcolorcode                suspicious-package
+discord                    qlimagesize                telegram
+firefox                    qlstephen                  viber
+github                     qlvideo                    visual-studio-code
+google-backup-and-sync     quicklook-json             wechat
+google-chrome              quicklookase               whatsapp
+java                       scroll-reverser
+jetbrains-toolbox          spectacle
+
+# Apply Mackup
+mackup restore
+
